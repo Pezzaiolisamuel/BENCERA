a100dc5d3beb00a4c4a67ff6b3da9a535eb458d3
Pezzaiolisamuel <pezzaiolisamuel@gmail.com>
2026-01-22 16:00:44 +0100

    cleaning old repos and files, redirect landing page to db

--- bencera/src/app/page.tsx
+++ bencera/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { prisma } from "../lib/prism";
 import GalleryViewport from "@/components/GalleryViewport";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function HomePage() {
   const items = await prisma.item.findMany();
 
-  // Parse JSON fields
   const parsedItems = items.map((item) => ({
     ...item,
     availableColors: JSON.parse(item.availableColors || "[]"),
